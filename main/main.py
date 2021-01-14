9c2820ce8f0a2188dda4e787ba70deb33cec6217
genisyskernel <eduardo.doege@outlook.com>
2021-01-14 02:43:32 -0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,4 +1,22 @@
 # CS:GO TOOLS #
 
+# Imports #
 from steam.client import SteamClient
 from csgo.client import CSGOClient
+
+
+client = SteamClient()
+cs = CSGOClient(client)
+
+@client.on('logged_on')
+def start_csgo():
+    cs.launch()
+
+@cs.on('ready')
+def gc_ready():
+    # send messages to gc
+    pass
+
+client.cli_login()
+
+client.run_forever()
